1f4b997f9bf5ba21101169568dc642d8f3eb1402
altunin96 <nilaktunin96@gmail.com>
2025-03-26 02:08:36 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void whenCounter0And10Then55() {
+    void whenSumCounter0And10Then55() {
         int start = 0;
         int finish = 10;
         int expected = 55;
@@ -17,7 +17,7 @@ class CounterTest {
     }
 
     @Test
-    void whenCounter3And8Then33() {
+    void whenSumCounter3And8Then33() {
         int start = 3;
         int finish = 8;
         int expected = 33;
@@ -26,7 +26,7 @@ class CounterTest {
     }
 
     @Test
-    void whenCounter1And1Then1() {
+    void whenSumCounter1And1Then1() {
         int start = 1;
         int finish = 1;
         int expected = 1;
@@ -35,11 +35,65 @@ class CounterTest {
     }
 
     @Test
-    void whenCounter4And2Then0() {
+    void whenSumCounter4And2Then0() {
         int start = 4;
         int finish = 2;
         int expected = 0;
         int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFrom0To10Then30() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFrom3To8Then18() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFrom1To1Then1() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFromMinus1To3Then2() {
+        int start = -1;
+        int finish = 3;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFrom0To2Then2() {
+        int start = 0;
+        int finish = 2;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenNumbersFromMinus4To2ThenMinus4() {
+        int start = -4;
+        int finish = 2;
+        int result = Counter.sumByEven(start, finish);
+        int expected = -4;
+        assertThat(result).isEqualTo(expected);
     }
 }
